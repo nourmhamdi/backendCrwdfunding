83f5262f25fbe56730a35c5486abbf89f2a0a02c
nourmhamdi <nour.mhamdi12@gmail.com>
2019-05-21 12:15:14 +0100

    first Commit backened

---
+++ src/main/java/crowdfunding/dapp/crowdfundingdapp/CrowdfundingdappApplication.java
@@ -0,0 +1,13 @@
+package crowdfunding.dapp.crowdfundingdapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdfundingdappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrowdfundingdappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/crowdfunding/dapp/crowdfundingdapp/CrowdfundingdappApplicationTests.java
@@ -0,0 +1,16 @@
+package crowdfunding.dapp.crowdfundingdapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrowdfundingdappApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
